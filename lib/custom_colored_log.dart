38a35d86a15cb6f6eb53fd74a0c2234d457103b3
shadat rahman <shadat.rahman.464@gmail.com>
2023-01-17 00:09:28 +0600

    logout

--- lib/custom_colored_log.dart
+++ lib/custom_colored_log.dart
@@ -1,19 +1,19 @@
-import 'dart:developer' as developer;
+import 'dart:developer' as devtools;
 
 class CustomColoredLogs {
   static logInfo(var msg) {
-    developer.log('\x1B[34m$msg\x1B[0m');
+    devtools.log('\x1B[34m$msg\x1B[0m');
   }
 
   static logSuccess(var msg) {
-    developer.log('\x1B[32m$msg\x1B[0m');
+    devtools.log('\x1B[32m$msg\x1B[0m');
   }
 
   static logWarning(var msg) {
-    developer.log('\x1B[33m$msg\x1B[0m');
+    devtools.log('\x1B[33m$msg\x1B[0m');
   }
 
   static logError(var msg) {
-    developer.log('\x1B[31m$msg\x1B[0m');
+    devtools.log('\x1B[31m$msg\x1B[0m');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,16 +46,88 @@ class _HomePageState extends State<HomePage> {
             if (user != null) {
               if (user.emailVerified) {
                 CustomColoredLogs.logSuccess('Email Verified');
+                return const NotesView();
               } else {
                 CustomColoredLogs.logSuccess('Email is not verified');
                 return const VerifyEmailView();
               }
+            } else {
+              CustomColoredLogs.logSuccess('User is null');
+              return const LoginView();
             }
-            return const Text('Done');
           default:
             return const CircularProgressIndicator();
         }
       },
     );
   }
+}
+
+enum MenuAction { logout }
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogOut = await showLogOutDialog(context);
+                  if (shouldLogOut) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/login/',
+                      (route) => false,
+                    );
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('Log out'),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Main UI'),
+      ),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign out'),
+        content: const Text('Do you really want to sign out?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Log out'),
+          ),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
 }
