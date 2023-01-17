9713318637986cc543d6927d59f754afb11627e4
shadat rahman <shadat.rahman.464@gmail.com>
2023-01-18 00:19:47 +0600

    log in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/custom_colored_log.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
@@ -44,6 +45,7 @@ class _HomePageState extends State<HomePage> {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
             final user = FirebaseAuth.instance.currentUser;
+            CustomColoredLogs.logInfo(user);
             if (user != null) {
               if (user.emailVerified) {
                 return const NotesView();
